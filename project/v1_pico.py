d011fc8a4b9a6e5197c2eca2506b0a180edaa70b
mojothejojo123 <161074684+mojothejojo123@users.noreply.github.com>
2024-05-10 11:00:36 -0700

    Update v1_pico.py

--- project/v1_pico.py
+++ project/v1_pico.py
@@ -3,7 +3,7 @@ from machine import PWM
 import time
 
 led = Pin("LED", Pin.OUT)
-led.on()
+#led.on()
 uart = UART(0, baudrate=9600, tx=Pin(16), rx=Pin(17))
 uart.init(bits=8, parity=None, stop=1)
 
@@ -17,37 +17,24 @@ print("Pico UART initialized, waiting for data...")
 pwmMotor.duty_ns(1500000)
 pwmServo.duty_ns(1500000)
 time.sleep(0.5)
+pwmMotor.duty_ns(1520000)
+pwmServo.duty_ns(1700000)
+time.sleep(0.5)
+pwmMotor.duty_ns(1540000)
+pwmServo.duty_ns(1500000)
+time.sleep(0.5)
+pwmMotor.duty_ns(1560000)
+pwmServo.duty_ns(1300000)
+time.sleep(0.5)
+pwmMotor.duty_ns(1565000)
+time.sleep(0.5)
+pwmMotor.duty_ns(1570000)
+time.sleep(0.5)
 
-#pwmMotor.duty_ns(1520000)
-#pwmServo.duty_ns(1700000)
-#time.sleep(0.5)
 
-#pwmMotor.duty_ns(1540000)
-#pwmServo.duty_ns(1900000)
-#time.sleep(0.5)
-# 
-# pwmMotor.duty_ns(1560000)
-# pwmServo.duty_ns(1700000)
-# time.sleep(0.5)
-# 
-# pwmMotor.duty_ns(1580000)
-# pwmServo.duty_ns(1500000)
-# time.sleep(0.5)
-# 
-# pwmMotor.duty_ns(1600000)
-# pwmServo.duty_ns(1300000)
-# time.sleep(0.5)
-# 
-# pwmMotor.duty_ns(1600000)
-# pwmServo.duty_ns(1100000)
-# time.sleep(0.5)
-# 
-# pwmMotor.duty_ns(1600000)
-# pwmServo.duty_ns(1500000)
-# time.sleep(0.5)
-# 
-# #pwmServo.duty_ns(1500000)
-# print("Motor test done")
+#pwmServo.duty_ns(1500000)
+print("Motor test done")
+led.on()
 
 
 while True:
@@ -59,36 +46,41 @@ while True:
             print("Received from Pi 4:", char)
 
             if char=="F":
+                led.toggle()
                 print("Forward")
                 #pwmMotor.duty_ns(1550000) 
-                pwmMotor.duty_ns(1570000)
+                pwmMotor.duty_ns(1577000)
                 pwmServo.duty_ns(1500000)
                 #time.sleep(2)  # Short delay to avoid using too much CPU
             elif char=="L":
+                led.toggle()
                 print("Left")
                 #pwmMotor.duty_ns(1550000) 
-                pwmMotor.duty_ns(1570000) 
-                pwmServo.duty_ns(1900000)
+                pwmMotor.duty_ns(1577000)
+                pwmServo.duty_ns(1200000)
                 #time.sleep(2)  # Short delay to avoid using too much CPU
             #elif char=="s":
                # print("Brake")
                 #pwmMotor.duty_ns(1500000) 
                # pwmServo.duty_ns(1500000) 
             elif char=="R":
+                led.toggle()
                 print("Right")
                 #pwmMotor.duty_ns(1550000)
-                pwmMotor.duty_ns(1570000) 
-                pwmServo.duty_ns(1100000)
+                pwmMotor.duty_ns(1577000)
+                pwmServo.duty_ns(1800000)
+
                 #time.sleep(0.1)  # Short delay to avoid using too much CPU
            # elif char=="x":
              #   print("Backward")
              #   pwmMotor.duty_ns(1500000) 
              #   pwmMotor.duty_ns(1400000) 
              #   pwmServo.duty_ns(1500000)
-            else :
-                pwmMotor.duty_ns(1570000)
-                pwmServo.duty_ns(1500000)
+            #else:
+                #pwmMotor.duty_ns(1500000)
+                #pwmServo.duty_ns(1500000)
     time.sleep(0.1)  # Short delay to avoid using too much CPU
 
 else:
     print("error reading uart")
+
